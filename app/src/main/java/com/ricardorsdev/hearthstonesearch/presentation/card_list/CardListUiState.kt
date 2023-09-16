3de38e8eb314804597ecde5b674f0670380c47f3
Ricardo Sousa <ricardo.sousa@metait.ca>
2023-09-16 19:10:12 +0200

    Adding basic implementation for CardListViewModel

---
+++ app/src/main/java/com/ricardorsdev/hearthstonesearch/presentation/card_list/CardListUiState.kt
@@ -0,0 +1,9 @@
+package com.ricardorsdev.hearthstonesearch.presentation.card_list
+
+import com.ricardorsdev.hearthstonesearch.domain.model.Card
+
+sealed interface CardListUiState {
+    object Loading : CardListUiState
+    data class Error(val error: String) : CardListUiState
+    data class Success(val items: List<Card> = emptyList()) : CardListUiState
+}
---
+++ app/src/main/java/com/ricardorsdev/hearthstonesearch/presentation/card_list/CardListViewModel.kt
@@ -0,0 +1,47 @@
+package com.ricardorsdev.hearthstonesearch.presentation.card_list
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ricardorsdev.hearthstonesearch.domain.model.Resource
+import com.ricardorsdev.hearthstonesearch.domain.use_case.get_cards.GetCardsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CardListViewModel @Inject constructor(
+    private val getCardsUseCase: GetCardsUseCase
+): ViewModel() {
+
+    private var _state = mutableStateOf<CardListUiState>(CardListUiState.Loading)
+    val state = _state
+
+    init {
+        getCards()
+    }
+
+    private fun getCards() {
+        getCardsUseCase().onEach { resource ->
+            when (resource) {
+                is Resource.Loading -> {
+                    _state.value = CardListUiState.Loading
+                }
+
+                is Resource.Success -> {
+                    _state.value = CardListUiState.Success(
+                        items = resource.data ?: emptyList()
+                    )
+                }
+
+                is Resource.Error -> {
+                    _state.value = CardListUiState.Error(
+                        error = resource.message ?: "An unexpected error occurred"
+                    )
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
